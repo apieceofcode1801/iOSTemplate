0362440e1ddfc9daf45cbfeb87a19987a5492d2a
apieceofcode1801 <apieceofcode1801@gmail.com>
2021-09-06 22:18:24 +0700

    integrate to sample app

--- iOSTemplate/ViewController.swift
+++ iOSTemplate/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import iOSTemplate
 
 class ViewController: UIViewController {
     
---
+++ iOSTemplate/iOSTemplate.h
@@ -0,0 +1,18 @@
+//
+//  iOSTemplate.h
+//  iOSTemplate
+//
+//  Created by Trung Hoang on 06/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSTemplate.
+FOUNDATION_EXPORT double iOSTemplateVersionNumber;
+
+//! Project version string for iOSTemplate.
+FOUNDATION_EXPORT const unsigned char iOSTemplateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSTemplate/PublicHeader.h>
+
+
