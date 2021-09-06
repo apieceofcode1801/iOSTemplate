b39deb12677dcad4fe15e92678e78ba31da0043b
apieceofcode1801 <apieceofcode1801@gmail.com>
2021-09-06 22:36:10 +0700

    move files to root

---
+++ iOSTemplate/Sources/iOSTemplate.framework/Headers/iOSTemplate_Framework.h
@@ -0,0 +1,18 @@
+//
+//  iOSTemplate_Framework.h
+//  iOSTemplate_Framework
+//
+//  Created by Trung Hoang on 05/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSTemplate_Framework.
+FOUNDATION_EXPORT double iOSTemplate_FrameworkVersionNumber;
+
+//! Project version string for iOSTemplate_Framework.
+FOUNDATION_EXPORT const unsigned char iOSTemplate_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSTemplate_Framework/PublicHeader.h>
+
+
